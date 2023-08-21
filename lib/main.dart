eeb0f5af637f315893257025092a99b8e3ec9c64
doggbmx <manuelorticosta@gmail.com>
2023-08-21 19:43:55 -0400

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      title: 'Toktik',
+      title: 'TikMock',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
