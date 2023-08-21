2b4b038ea01ce2882c081b0c78381ef31f22d2b0
doggbmx <manuelorticosta@gmail.com>
2023-08-21 19:55:48 -0400

    MultiProvider implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+
+import 'presentation/screens/discover/discover_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,17 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
-      title: 'TikMock',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<DiscoverProvider>(
+          create: (_) => DiscoverProvider(),
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        title: 'TikMock',
+        home: const DiscoverScreen(),
       ),
     );
   }
