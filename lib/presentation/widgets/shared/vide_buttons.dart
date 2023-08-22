3c8315cd555b2e2173221dd339d6880f03dc50fb
doggbmx <manuelorticosta@gmail.com>
2023-08-21 21:11:54 -0400

    implement animation

--- lib/presentation/widgets/shared/vide_buttons.dart
+++ lib/presentation/widgets/shared/vide_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -19,6 +20,15 @@ class VideoButtons extends StatelessWidget {
         _CustomIconButton(
           value: video.views,
           icon: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0,
+            icon: Icons.play_circle_outline,
+          ),
         ),
       ],
     );
@@ -48,9 +58,10 @@ class _CustomIconButton extends StatelessWidget {
             size: 30,
           ),
         ),
-        Text(
-          HumanFormats.humanReadbleNumber(value.toDouble()),
-        )
+        if (value > 0)
+          Text(
+            HumanFormats.humanReadbleNumber(value.toDouble()),
+          )
       ],
     );
   }
