de8eaf21d98a40577adea49c8491b12506c96bf2
doggbmx <manuelorticosta@gmail.com>
2023-08-21 20:15:19 -0400

    add logic on loadNextPage

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_posts.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  // TODO: Repository, DataSource
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // TODO: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
 
+    final List<VideoPost> newVideos = videoPosts
+        .map(
+          (e) => LocalVideoModel.fromJsonMap(e).toVideoPostEntity(),
+        )
+        .toList();
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
