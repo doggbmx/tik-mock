7a420749d40577bda5abaad04faacc7395adfcb5
doggbmx <manuelorticosta@gmail.com>
2023-08-24 10:34:27 -0400

    VP repository implementation

---
+++ lib/infrastructure/repositories/video_post_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:toktik/domain/datasources/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostRepositoryImplementation implements VideoPostRepository {
+  final VideoPostDataSource videoPostDataSource;
+
+  VideoPostRepositoryImplementation({required this.videoPostDataSource});
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videoPostDataSource.getTrendingVideosByPage(page);
+  }
+}
