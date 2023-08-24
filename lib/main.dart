dbfd27d33462f2fb345232d721582bb37200d2d2
doggbmx <manuelorticosta@gmail.com>
2023-08-24 10:54:26 -0400

    adapt main function repository & datasource

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_post_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 
 import 'presentation/screens/discover/discover_screen.dart';
@@ -12,12 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostDataSource = LocalVideoDataSourceImplementation();
+    final videoPostRepository = VideoPostRepositoryImplementation(
+        videoPostDataSource: videoPostDataSource);
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<DiscoverProvider>(
           // FORCE LOAD
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage(),
+          create: (_) => DiscoverProvider(
+            videosRepository: videoPostRepository,
+          )..loadNextPage(),
         ),
       ],
       child: MaterialApp(
