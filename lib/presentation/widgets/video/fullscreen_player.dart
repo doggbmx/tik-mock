341c8448f5cf907b3555cd81097536f58f238e5c
doggbmx <manuelorticosta@gmail.com>
2023-08-23 20:54:01 -0400

    add GestureDetector to VideoPlayer

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -46,20 +46,29 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             ),
           );
         } else {
-          return AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(
-              children: [
-                VideoPlayer(controller),
-                // gradiente
-                // texto
-                Positioned(
-                    bottom: 50,
-                    left: 20,
-                    child: _VideoCaption(
-                      caption: widget.caption,
-                    ))
-              ],
+          return GestureDetector(
+            onTap: () {
+              if (controller.value.isPlaying) {
+                controller.pause();
+                return;
+              }
+              controller.play();
+            },
+            child: AspectRatio(
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  VideoPlayer(controller),
+                  // gradiente
+                  // texto
+                  Positioned(
+                      bottom: 50,
+                      left: 20,
+                      child: _VideoCaption(
+                        caption: widget.caption,
+                      ))
+                ],
+              ),
             ),
           );
         }
